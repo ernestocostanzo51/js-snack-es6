2b7fc9293d16edd99ab8235433c178c3ec4003af
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-02-17 17:38:14 +0100

    commit secondo snack

--- main.js
+++ main.js
@@ -30,13 +30,52 @@ for(i=0; i<bici.length; i++){
        if (bici[i].peso < pesoMin.peso) {
         pesoMin = bici[i];
     }
-        
-    }
-
-
-
-
+    } 
 console.log(`la bici con peso minore è ${pesoMin.nome} con un peso di ${pesoMin.peso}`)
 
 
 //snack2
+
+let squadre = [
+
+    {
+        nome:"juventus",
+        punti: 0,
+        falliSubiti:0,
+    },
+    {
+        nome:"palermo",
+        punti: 0,
+        falliSubiti:0,
+    },
+    {
+        nome:"catania",
+        punti: 0,
+        falliSubiti:0,
+    },
+    {
+        nome:"inter",
+        punti: 0,
+        falliSubiti:0,
+    },
+]
+
+
+
+
+
+ 
+function random() {
+    return Math.floor(Math.random()*10) 
+}
+
+for(i=0; i<squadre.length; i++){
+    squadre[i].punti = random()
+    squadre[i].falliSubiti = random()
+}
+
+
+
+
+
+console.log(squadre)
